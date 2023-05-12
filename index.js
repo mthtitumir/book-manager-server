39eb9d81978ea3e5876403b5742083eb1831ce23
mthtitumir <titumirgccc@gmail.com>
2023-05-12 17:11:17 +0600

    data ready

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
 
+
+//middleware
 app.use(cors());
 app.use(express.json());
 
@@ -25,6 +29,49 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
+        const bookCollection = client.db("bookManager").collection("books");
+
+        //insert a book to db
+        app.get("/health", (req, res) => {
+            res.send("All is well!")
+        })
+        app.post('/upload-book', async (req, res) => {
+            const data = req.body;
+            console.log(body);
+            const result = await bookCollection.insertOne(data);
+            res.send(result);
+        })
+
+        app.get('/all-books', async (req, res) => {
+            const books = bookCollection.find();
+            const result = await books.toArray();
+            res.send(result);
+        })
+
+        app.patch('/book/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const updatedBookData = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    ...updatedBookData
+                }
+            }
+            const result = await bookCollection.updateOne(filter, updatedDoc)
+            res.send(result);
+        })
+
+        app.delete('book/:id', async(req, res) =>{
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await bookCollection.deleteOne(filter);
+            res.send(result);
+        })
+
+
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
@@ -35,3 +82,11 @@ async function run() {
 run().catch(console.dir);
 
 //mongodb code
+
+
+app.get('/', (req, res) => {
+    res.send('books are selling')
+})
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
